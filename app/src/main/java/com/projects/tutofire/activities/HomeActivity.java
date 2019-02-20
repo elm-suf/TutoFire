8f99438a5f1c9d46df89481c8a1ba3a1030bbb86
elm-suf <elm.sufian@gmail.com>
2019-02-20 18:28:53 +0100

    setup home page

--- app/src/main/java/com/projects/tutofire/activities/HomeActivity.java
+++ app/src/main/java/com/projects/tutofire/activities/HomeActivity.java
@@ -15,7 +15,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Intent intent = new Intent(this, RegisterActivity.class);
+        Intent intent = new Intent(this, Home.class);
         btn_goto_register = findViewById(R.id.btn_goto_register);
 
         btn_goto_register.setOnClickListener(v -> {
